cfc6813d02f171287b6bd516bb4c3d2968a5476b
Karan Dhareshwar <karan@casperlabs.io>
2025-07-15 13:39:12 -0500

    Extend guardrails for put-transaction

--- src/transaction/put.rs
+++ src/transaction/put.rs
@@ -2,8 +2,9 @@ use async_trait::async_trait;
 use clap::{ArgMatches, Command};
 use serde::{Deserialize, Serialize};
 
-use casper_client::cli::{parse, CliError, PublicKey, TransactionBuilderParams, TransactionStrParams, get_maybe_secret_key};
-use casper_types::{ActivationPoint, CLValue, CoreConfig, HighwayConfig, ProtocolVersion, PublicKey, StorageCosts, SystemConfig, TransactionConfig, VacancyConfig, WasmConfig, U512, HashAddr, Key};
+use casper_client::cli::{parse, CliError, PublicKey, TransactionBuilderParams, TransactionStrParams, get_maybe_secret_key, query_global_state};
+use casper_types::{ActivationPoint, CLValue, CoreConfig, HighwayConfig, ProtocolVersion, PublicKey, StorageCosts, SystemConfig, TransactionConfig, VacancyConfig, WasmConfig, U512, HashAddr, Key, SystemHashRegistry};
+use casper_client::Verbosity;
 
 use super::creation_common::{
     activate_bid, add_bid, add_reservations, cancel_reservations, change_bid_public_key, delegate,
@@ -403,6 +404,16 @@ async fn put_by_package_hash_transaction(arg_matches: &ArgMatches) -> Result<Suc
     let verbosity_level = common::verbose::get(arg_matches);
 
     let (transaction_builder_params, transaction_str_params) = package::run(arg_matches)?;
+    if let TransactionBuilderParams::PackageWithMajorVersion { package_hash, entry_point, .. } = transaction_builder_params {
+        let hash_addr = package_hash.value();
+        let entry_point = entry_point.to_string();
+        let min_bid_override = min_bid_override::get(arg_matches);
+        let args_as_json = transaction_str_params.session_args_json.to_string();
+        let simple_args = transaction_str_params.session_args_simple.clone();
+        check_auction_state_for_withdraw(node_address, verbosity_level, hash_addr, min_bid_override,  entry_point, args_as_json, simple_args,
+        ).await?
+    }
+
     casper_client::cli::put_transaction(
         rpc_id,
         node_address,
@@ -420,6 +431,44 @@ async fn put_package_by_name_transaction(arg_matches: &ArgMatches) -> Result<Suc
     let verbosity_level = common::verbose::get(arg_matches);
 
     let (transaction_builder_params, transaction_str_params) = package_alias::run(arg_matches)?;
+    if let TransactionBuilderParams::PackageAliasWithMajorVersion { package_alias, entry_point , .. } = transaction_builder_params {
+        let account_key = {
+            let secret_key = get_maybe_secret_key(transaction_str_params.secret_key, false, "")
+                .unwrap()
+                .unwrap();
+            Key::Account(PublicKey::from(&secret_key)
+                .to_account_hash())
+        };
+        let cl_value = casper_client::cli::query_global_state(rpc_id, node_address, verbosity_level, "", "", &account_key.to_formatted_string(), package_alias)
+            .await?
+            .result
+            .stored_value
+            .as_cl_value()
+            .unwrap();
+        let key = CLValue::to_t::<Key>(cl_value).map_err(|err| CliError::InvalidCLValue(err.to_string()))?;
+        match key {
+            Key::Hash(hash_addr) => {
+                let entry_point = entry_point.to_string();
+                let min_bid_override = min_bid_override::get(arg_matches);
+                let args_as_json = transaction_str_params.session_args_json.to_string();
+                let simple_args = transaction_str_params.session_args_simple.clone();
+                check_auction_state_for_withdraw(node_address, verbosity_level, hash_addr, min_bid_override,  entry_point, args_as_json, simple_args,
+                ).await?
+            }
+            Key::SmartContract(addr) => {
+                let hash_addr = addr;
+                let entry_point = entry_point.to_string();
+                let min_bid_override = min_bid_override::get(arg_matches);
+                let args_as_json = transaction_str_params.session_args_json.to_string();
+                let simple_args = transaction_str_params.session_args_simple.clone();
+                check_auction_state_for_withdraw(node_address, verbosity_level, hash_addr, min_bid_override,  entry_point, args_as_json, simple_args,
+                ).await?
+            }
+            _ => {}
+        }
+    }
+
+
     casper_client::cli::put_transaction(
         rpc_id,
         node_address,
@@ -526,65 +575,78 @@ async fn check_auction_state_for_withdraw(
         // First check if we are calling the auction.
         if auction_hash_addr == hash_addr {
             // Now parse the args for the amount to do the value check.
-            if let Some(runtime_args) =
-                parse::args_json::session::parse(&session_args_as_json)?
-            {
-                match runtime_args.get("amount").map(|cl| {
-                    CLValue::to_t::<U512>(cl)
-                        .map_err(|err| CliError::InvalidCLValue(err.to_string()))?
-                }) {
-                    Some(amount) => {
-                        let public_key = runtime_args
-                            .get("public_key")
-                            .map(|cl| {
-                                CLValue::to_t::<PublicKey>(cl)
-                                    .map_err(|err| CliError::InvalidCLValue(err.to_string()))?
-                            })
-                            .unwrap();
-                        return do_withdraw_amount_checks(
-                            node_address,
-                            verbosity_level,
-                            public_key,
-                            amount,
-                            min_bid_override,
-                        )
-                            .await;
-                    }
-                    None => {
-                        println!("no amount arg found, skipping withdraw check")
-                    }
-                };
-            };
-            if let Some(runtime_args) =
-                parse::arg_simple::session::parse(&session_args_simple)?
-            {
-                match runtime_args.get("amount").map(|cl| {
-                    CLValue::to_t::<U512>(cl)
-                        .map_err(|err| CliError::InvalidCLValue(err.to_string()))?
-                }) {
-                    Some(amount) => {
-                        let public_key = runtime_args
-                            .get("public_key")
-                            .map(|cl| {
-                                CLValue::to_t::<PublicKey>(cl)
-                                    .map_err(|err| CliError::InvalidCLValue(err.to_string()))?
-                            })
-                            .unwrap();
-                        return do_withdraw_amount_checks(
-                            node_address,
-                            verbosity_level,
-                            public_key,
-                            amount,
-                            min_bid_override,
-                        )
-                            .await
-                    }
-                    None => {
-                        println!("no amount arg found, skipping withdraw check")
-                    }
-                };
-            };
+        } else {
+            // check if the hash addr matches the package hash addr on the contract itself.
+            let key = Key::Hash(auction_hash_addr);
+            let package_addr = query_global_state("", node_address, verbosity_level, "", "", &key, "")
+                .await?
+                .result
+                .stored_value
+                .as_contract()
+                .ok_or_else(CliError::FailedToGetSystemHashRegistry)?.contract_package_hash().value();
+            if (package_addr != hash_addr) {
+                return Ok(())
+            }
         }
+
+        if let Some(runtime_args) =
+            parse::args_json::session::parse(&session_args_as_json)?
+        {
+            match runtime_args.get("amount").map(|cl| {
+                CLValue::to_t::<U512>(cl)
+                    .map_err(|err| CliError::InvalidCLValue(err.to_string()))?
+            }) {
+                Some(amount) => {
+                    let public_key = runtime_args
+                        .get("public_key")
+                        .map(|cl| {
+                            CLValue::to_t::<PublicKey>(cl)
+                                .map_err(|err| CliError::InvalidCLValue(err.to_string()))?
+                        })
+                        .unwrap();
+                    return do_withdraw_amount_checks(
+                        node_address,
+                        verbosity_level,
+                        public_key,
+                        amount,
+                        min_bid_override,
+                    )
+                        .await;
+                }
+                None => {
+                    println!("no amount arg found, skipping withdraw check")
+                }
+            };
+        };
+        if let Some(runtime_args) =
+            parse::arg_simple::session::parse(&session_args_simple)?
+        {
+            match runtime_args.get("amount").map(|cl| {
+                CLValue::to_t::<U512>(cl)
+                    .map_err(|err| CliError::InvalidCLValue(err.to_string()))?
+            }) {
+                Some(amount) => {
+                    let public_key = runtime_args
+                        .get("public_key")
+                        .map(|cl| {
+                            CLValue::to_t::<PublicKey>(cl)
+                                .map_err(|err| CliError::InvalidCLValue(err.to_string()))?
+                        })
+                        .unwrap();
+                    return do_withdraw_amount_checks(
+                        node_address,
+                        verbosity_level,
+                        public_key,
+                        amount,
+                        min_bid_override,
+                    )
+                        .await
+                }
+                None => {
+                    println!("no amount arg found, skipping withdraw check")
+                }
+            };
+        };
     }
     Ok(())
 }
